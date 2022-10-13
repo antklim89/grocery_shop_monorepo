30133a4cedeab34f2a15ffbd9a622ab56c3b1d0b
Anton <antklim89@mail.ru>
2022-10-13 20:37:33 +0300

    move database

--- backend/config/database.ts
+++ backend/config/database.ts
@@ -1,11 +1,12 @@
 import path from 'path';
 
+
 export default ({ env }) => ({
-  connection: {
-    client: 'sqlite',
     connection: {
-      filename: path.join(__dirname, '..', '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+        client: 'sqlite',
+        connection: {
+            filename: path.join(__dirname, '..', '..', env('DATABASE_FILENAME', 'database/data.db')),
+        },
+        useNullAsDefault: true,
     },
-    useNullAsDefault: true,
-  },
 });
