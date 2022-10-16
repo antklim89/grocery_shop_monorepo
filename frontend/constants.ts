8b078b1845b1743272da9fe66a18e0ed586ee425
Anton <antklim89@mail.ru>
2022-10-16 10:17:51 +0300

    update

--- frontend/constants.ts
+++ frontend/constants.ts
@@ -8,13 +8,7 @@ export const CART_LOCAL_STORAGE_NAME = process.env.CART_LOCAL_STORAGE_NAME || 'g
 export const AUTH_TOKEN_NAME = process.env.AUTH_TOKEN_NAME || 'token';
 export const PRODUCTS_LIMIT = Number(process.env.PRODUCTS_LIMIT) || 6;
 
-export const ORDER_EXPIRE_TIME = 1000 * 60 * 15;
-
 export const API_URL = process.env.NEXT_PUBLIC_API_URL || (() => {
     throw new Error('NEXT_PUBLIC_API_URL env variable is required.');
 })();
-export const IMAGE_URL = process.env.NEXT_PUBLIC_IMAGE_URL || (() => {
-    throw new Error('IMAGE_URL env variable is required.');
-})();
-
 
--- frontend/next.config.js
+++ frontend/next.config.js
@@ -3,19 +3,10 @@ module.exports = {
     compress: true,
     images: {
         domains: [
-            process.env.NEXT_PUBLIC_API_URL.replace(/http(s|):\/\/([\w\d.]*)(:|)(\d*)/, '$2'),
+            new URL(process.env.NEXT_PUBLIC_API_URL).host,
             '192.168.90.19',
             'localhost',
-            process.env.NEXT_PUBLIC_IMAGE_URL,
         ],
-    },
-    webpack: (config) => {
-        config.module.rules.push({
-            test: /\.(graphql|gql)$/,
-            exclude: /node_modules/,
-            loader: 'graphql-tag/loader',
-        });
-        return config;
     },
 };
 
