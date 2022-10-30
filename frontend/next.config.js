c267d391624b46f12e1ff9bd2721c10956e793e5
Anton <antklim89@mail.ru>
2022-10-30 10:22:17 +0300

    update images domain

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -2,11 +2,6 @@ module.exports = {
     reactStrictMode: true,
     compress: true,
     images: {
-        domains: [
-            new URL(process.env.NEXT_PUBLIC_API_URL).host,
-            '192.168.90.19',
-            'localhost',
-        ],
+        domains: [new URL(process.env.NEXT_PUBLIC_API_URL).hostname],
     },
 };
-
