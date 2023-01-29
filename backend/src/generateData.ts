45ae5460d20c42d412f51006faddd08e89e36baf
Anton <antklim89@mail.ru>
2023-01-29 22:44:19 +0300

    add random data generator

---
+++ backend/src/generateData.ts
@@ -0,0 +1,105 @@
+import { faker } from '@faker-js/faker';
+import { Strapi } from '@strapi/strapi';
+import { times, sample, sampleSize, random } from 'lodash';
+
+
+const FEATURES_COUNT = 3;
+const PRODUCTS_COUNT = 100;
+const categoriesList = ['Vegetables', 'Fruits', 'Meat', 'Bakery', 'Milk products', 'Ready-made food'];
+const countriesList = ['Russia', 'USA', 'Egypt', 'Turkey', 'Azerbaijan', 'China', 'Cuba', 'Greece', 'India', 'Mexico'];
+
+export interface Image {
+    id: number;
+    name: string;
+    url: string;
+}
+
+
+export async function generateData(strapi: Strapi) {
+    const images = await strapi.plugins.upload.services.upload.findMany({});
+    await generateFeatures(images);
+    const countries = await generateCountries();
+    const categories = await generateCategories();
+    await generateProducts(images, categories, countries);
+    await generateAbout(images);
+    await generateHero(images);
+}
+
+async function generateHero(images: Image[]) {
+    const hero = await strapi.db.query('api::hero.hero').findMany({});
+    if (hero.length > 0) return;
+    await strapi.db.query('api::hero.hero').create({
+        data: {
+            title: '100% natural',
+            text: faker.lorem.paragraph(2),
+            image: images.find((img) => img.name.includes('hero')),
+        },
+    });
+}
+
+
+async function generateAbout(images: Image[]) {
+    const about = await strapi.db.query('api::about.about').findMany({});
+    if (about.length > 0) return;
+    await strapi.db.query('api::about.about').create({
+        data: {
+            body: faker.lorem.paragraph(3),
+            image: images.find((img) => img.name.startsWith('placeholder_2')),
+        },
+    });
+}
+
+async function generateFeatures(images: Image[]) {
+    const features = await strapi.db.query('api::feature.feature').findMany({});
+    if (features.length > 0) return;
+    await Promise.all(times(FEATURES_COUNT, (i) => strapi.db.query('api::feature.feature').create({
+        data: {
+            title: faker.company.catchPhrase(),
+            text: faker.lorem.paragraph(),
+            image: images.find((img) => img.name.startsWith(`feature_${i + 1}`)) || images[0],
+        },
+    })));
+}
+
+async function generateCountries() {
+    const prevCountries = await strapi.db.query('api::country.country').findMany({});
+    if (prevCountries.length > 0) return prevCountries;
+    return Promise.all(countriesList.map((country) => strapi.db.query('api::country.country').create({
+        data: {
+            name: country,
+        },
+    })));
+}
+
+async function generateCategories() {
+    const prevCategories = await strapi.db.query('api::category.category').findMany({});
+    if (prevCategories.length > 0) return prevCategories;
+    return Promise.all(categoriesList.map((category) => strapi.db.query('api::category.category').create({
+        data: {
+            name: category,
+        },
+    })));
+}
+
+async function generateProducts(images: Image[], categories: { id: number; }[], countries: { id: number; }[]) {
+    const prevProducts = await strapi.db.query('api::product.product').findMany({});
+    if (prevProducts.length > 0) return prevProducts;
+
+    const date = faker.date.between('2002-01-01T10:30:00.354Z', '2023-01-29T18:38:51.354Z');
+    const units = ['kilogram', 'gram', 'liter', 'mililiter', 'piece', 'ton'];
+    return Promise.all(times(PRODUCTS_COUNT, () => strapi.db.query('api::product.product').create({
+        data: {
+            name: faker.commerce.productName(),
+            description: faker.lorem.paragraphs(3),
+            price: parseFloat(faker.commerce.price(100, 1000, 2)),
+            unit: sample(units),
+            discount: Math.random() > 0.3 ? random(10, 60, false) : 0,
+            createdAt: date,
+            updatedAt: date,
+            publishedAt: date,
+            category: sample(categories).id,
+            country: sample(countries).id,
+            images: sampleSize(images, 3),
+        },
+    })));
+}
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,5 +1,13 @@
+import { Strapi } from '@strapi/strapi';
+
+import { generateData } from './generateData';
+
 
 export default {
     register(/* { strapi }*/) { /** */ },
-    bootstrap(/* { strapi }*/) { /* */ },
+    async bootstrap({ strapi }: {strapi: Strapi}) {
+        await generateData(strapi);
+    },
 };
+
+
