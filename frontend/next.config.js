f46cd5a3cc645155d41251966287c000e77b939f
Anton <antklim89@mail.ru>
2023-02-14 20:21:08 +0300

    fix

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,7 +1,10 @@
+
+/** @type {import('next').NextConfig} */
 module.exports = {
     reactStrictMode: true,
     compress: true,
     images: {
         domains: [new URL(process.env.NEXT_PUBLIC_API_URL).hostname],
     },
+    staticPageGenerationTimeout: 240,
 };
