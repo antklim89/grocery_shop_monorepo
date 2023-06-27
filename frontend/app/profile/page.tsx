fa706a841b35524aa6f663323245968cede863ab
Anton <antklim89@mail.ru>
2023-06-27 15:16:10 +0300

    added profile page

---
+++ frontend/app/profile/page.tsx
@@ -0,0 +1,23 @@
+import { cookies } from 'next/headers';
+
+import Profile from '~/components/auth/Profile';
+import { getMe } from '~/requests';
+
+
+export const metadata = {
+    title: 'Profile',
+    openGraph: {
+        title: 'Profile',
+    },
+};
+
+const ProfilePage = async () => {
+    const token = cookies().get('token')?.value;
+    const user = token ? await getMe(token).catch(() => null) : null;
+
+    if (!user) return null;
+    return <Profile />;
+};
+
+
+export default ProfilePage;
