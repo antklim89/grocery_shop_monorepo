0a4fa796b66b5d41e9d77b53eb50bf2f2b189947
Anton <antklim89@mail.ru>
2023-07-03 16:39:11 +0300

    added NotFound page

---
+++ frontend/components/utils/NotFound.tsx
@@ -0,0 +1,13 @@
+
+
+const NotFound = () => {
+    return (
+        <div className="position-absolute top-50 start-50 translate-middle">
+            <span className="h1">
+                404 | PAGE NOT FOUND
+            </span>
+        </div>
+    );
+};
+
+export default NotFound;
