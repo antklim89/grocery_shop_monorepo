bf6123bf6a64791c2e6a29e76164d0e735a635fb
Anton <antklim89@mail.ru>
2023-07-12 15:52:48 +0300

    made componentn client

--- frontend/components/layout/HeaderLinks.tsx
+++ frontend/components/layout/HeaderLinks.tsx
@@ -1,8 +1,10 @@
+'use client';
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 
 import LogoutButton from '~/components/auth/LogoutButton';
-import Loading from '~/components/utils/Loading';
-import ProtectedComponent from '~/components/utils/ProtectedComponent';
+import NoSsr from '~/components/utils/NoSsr';
+import { useAuthStore } from '~/store';
 import { cls } from '~/utils/cls';
 
 
@@ -11,59 +13,57 @@ interface HeaderLinksProps {
 }
 
 const HeaderLinks = ({ className }: HeaderLinksProps): JSX.Element => {
-    const route = typeof window === 'undefined' ? '' : window.location.pathname;
+    const pathname = usePathname();
+    const user = useAuthStore((state) => state.user);
 
     return (
         <ul className={cls('navbar-nav justify-content-end flex-grow-1 pe-3', className)}>
             <li className="nav-item" data-bs-dismiss="offcanvas">
-                <Link passHref className={`nav-link ${route === '/' ? 'active' : ''}`} href="/">
+                <Link passHref className={`nav-link ${pathname === '/' ? 'active' : ''}`} href="/">
                     HOME
                 </Link>
             </li>
             <li className="nav-item" data-bs-dismiss="offcanvas">
-                <Link passHref className={`nav-link ${route === '/products' ? 'active' : ''}`} href="/product">
+                <Link passHref className={`nav-link ${pathname === '/product' ? 'active' : ''}`} href="/product">
                     PRODUCTS
                 </Link>
             </li>
             <li className="nav-item me-auto" data-bs-dismiss="offcanvas">
-                <Link passHref className={`nav-link ${route === '/about' ? 'active' : ''}`} href="/about">
+                <Link passHref className={`nav-link ${pathname === '/about' ? 'active' : ''}`} href="/about">
                     ABOUT
                 </Link>
             </li>
-            <ProtectedComponent
-                fallback={(
-                    <li className="nav-item" data-bs-dismiss="offcanvas">
-                        <button className="btn btn-link nav-link mx-5" type="button">
-                            <Loading loading size="sm" />
-                        </button>
-                    </li>
-                )}
-                render={(
-                    <>
-                        <li className="nav-item" data-bs-dismiss="offcanvas">
-                            <Link className={`nav-link ${route === '/signup' ? 'active' : ''}`} href="/signup">
-                                Sign Up
-                            </Link>
-                        </li>
-                        <li className="nav-item me-5" data-bs-dismiss="offcanvas">
-                            <Link className={`nav-link ${route === '/login' ? 'active' : ''}`} href="/login">
-                                Log In
-                            </Link>
-                        </li>
-                    </>
-                )}
-            >
-                <li className="nav-item me-1">
-                    <Link className="nav-link btn btn-link" href="/profile">
-                        Profile
-                    </Link>
-                </li>
-                <li className="nav-item me-5">
-                    <LogoutButton
-                        className="nav-link"
-                    />
-                </li>
-            </ProtectedComponent>
+            <NoSsr>
+                {user
+                    ? (
+                        <>
+                            <li className="nav-item me-1">
+                                <Link className="nav-link btn btn-link" href="/profile">
+                                    Profile
+                                </Link>
+                            </li>
+                            <li className="nav-item me-5">
+                                <LogoutButton
+                                    className="nav-link"
+                                />
+                            </li>
+                        </>
+                    )
+                    : (
+                        <>
+                            <li className="nav-item" data-bs-dismiss="offcanvas">
+                                <Link className={`nav-link ${pathname === '/signup' ? 'active' : ''}`} href="/signup">
+                                    Sign Up
+                                </Link>
+                            </li>
+                            <li className="nav-item me-5" data-bs-dismiss="offcanvas">
+                                <Link className={`nav-link ${pathname === '/login' ? 'active' : ''}`} href="/login">
+                                    Log In
+                                </Link>
+                            </li>
+                        </>
+                    )}
+            </NoSsr>
         </ul>
     );
 };
