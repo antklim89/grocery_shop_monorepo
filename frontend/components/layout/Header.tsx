90bcc43a8cdbe8a165776169d6830e9cb704f59c
Anton <antklim89@mail.ru>
2023-07-14 14:18:24 +0300

    refactored navbar toggler

--- frontend/components/layout/Header.tsx
+++ frontend/components/layout/Header.tsx
@@ -8,40 +8,24 @@ import CartButton from '~/components/cart/CartButton';
 const Header = async () => {
     return (
         <header >
-            <nav className="navbar navbar-dark bg-dark navbar-expand-lg shadow mb-4">
+            <nav className="navbar navbar-dark bg-dark navbar-expand-lg bg-body-dark">
                 <div className="container">
                     <Link passHref className="navbar-brand me-auto" href="/">
                         GROCERY SHOP
                     </Link>
+                    <CartButton />
                     <button
-                        aria-controls="offcanvasNavbar"
+                        aria-controls="navbarToggler"
+                        aria-expanded="false"
+                        aria-label="Toggle navigation"
                         className="navbar-toggler"
-                        data-bs-target="#offcanvasNavbar"
-                        data-bs-toggle="offcanvas"
+                        data-bs-target="#navbarToggler"
+                        data-bs-toggle="collapse"
                         type="button"
                     >
                         <span className="navbar-toggler-icon" />
                     </button>
-                    <div
-                        aria-labelledby="offcanvasNavbarLabel"
-                        className="offcanvas offcanvas-end text-bg-primary"
-                        id="offcanvasNavbar"
-                        tabIndex={-1}
-                    >
-                        <div className="offcanvas-header">
-                            <span className="offcanvas-title" id="offcanvasNavbarLabel" />
-                            <button
-                                aria-label="Close"
-                                className="btn-close"
-                                data-bs-dismiss="offcanvas"
-                                type="button"
-                            />
-                        </div>
-                        <div className="offcanvas-body mx-1">
-                            <HeaderLinks />
-                        </div>
-                    </div>
-                    <CartButton />
+                    <HeaderLinks />
                 </div>
             </nav>
         </header>
--- frontend/components/layout/HeaderLinks.tsx
+++ frontend/components/layout/HeaderLinks.tsx
@@ -17,54 +17,58 @@ const HeaderLinks = ({ className }: HeaderLinksProps): JSX.Element => {
     const user = useAuthStore((state) => state.user);
 
     return (
-        <ul className={cls('navbar-nav justify-content-end flex-grow-1 pe-3', className)}>
-            <li className="nav-item" data-bs-dismiss="offcanvas">
-                <Link passHref className={`nav-link ${pathname === '/' ? 'active' : ''}`} href="/">
-                    HOME
-                </Link>
-            </li>
-            <li className="nav-item" data-bs-dismiss="offcanvas">
-                <Link passHref className={`nav-link ${pathname === '/product' ? 'active' : ''}`} href="/product">
-                    PRODUCTS
-                </Link>
-            </li>
-            <li className="nav-item me-auto" data-bs-dismiss="offcanvas">
-                <Link passHref className={`nav-link ${pathname === '/about' ? 'active' : ''}`} href="/about">
-                    ABOUT
-                </Link>
-            </li>
-            <NoSsr>
-                {user
-                    ? (
-                        <>
-                            <li className="nav-item me-1">
-                                <Link className={`nav-link ${pathname === '/profile' ? 'active' : ''}`} href="/profile">
-                                    PROFILE
-                                </Link>
-                            </li>
-                            <li className="nav-item me-5">
-                                <LogoutButton
-                                    className="nav-link"
-                                />
-                            </li>
-                        </>
-                    )
-                    : (
-                        <>
-                            <li className="nav-item" data-bs-dismiss="offcanvas">
-                                <Link className={`nav-link ${pathname === '/signup' ? 'active' : ''}`} href="/signup">
-                                    Sign Up
-                                </Link>
-                            </li>
-                            <li className="nav-item me-5" data-bs-dismiss="offcanvas">
-                                <Link className={`nav-link ${pathname === '/login' ? 'active' : ''}`} href="/login">
-                                    Log In
-                                </Link>
-                            </li>
-                        </>
-                    )}
-            </NoSsr>
-        </ul>
+        <div className="collapse navbar-collapse" id="navbarToggler">
+            <ul
+                className={cls('navbar-nav justify-content-end flex-grow-1 pe-3', className)}
+            >
+                <li className="nav-item" data-target=".navbar-collapse.show" data-toggle="collapse">
+                    <Link passHref className={`nav-link ${pathname === '/' ? 'active' : ''}`} href="/" >
+                        HOME
+                    </Link>
+                </li>
+                <li className="nav-item">
+                    <Link passHref className={`nav-link ${pathname === '/product' ? 'active' : ''}`} href="/product">
+                        PRODUCTS
+                    </Link>
+                </li>
+                <li className="nav-item me-auto">
+                    <Link passHref className={`nav-link ${pathname === '/about' ? 'active' : ''}`} href="/about">
+                        ABOUT
+                    </Link>
+                </li>
+                <NoSsr>
+                    {user
+                        ? (
+                            <>
+                                <li className="nav-item me-1">
+                                    <Link className={`nav-link ${pathname === '/profile' ? 'active' : ''}`} href="/profile">
+                                        PROFILE
+                                    </Link>
+                                </li>
+                                <li className="nav-item me-5">
+                                    <LogoutButton
+                                        className="nav-link"
+                                    />
+                                </li>
+                            </>
+                        )
+                        : (
+                            <>
+                                <li className="nav-item">
+                                    <Link className={`nav-link ${pathname === '/signup' ? 'active' : ''}`} href="/signup">
+                                        Sign Up
+                                    </Link>
+                                </li>
+                                <li className="nav-item me-5">
+                                    <Link className={`nav-link ${pathname === '/login' ? 'active' : ''}`} href="/login">
+                                        Log In
+                                    </Link>
+                                </li>
+                            </>
+                        )}
+                </NoSsr>
+            </ul>
+        </div>
     );
 };
 
